6bde594365e177ff12787538cde8c4b573e75549
jrwebdev <web@jravenscroft.co.uk>
2017-01-12 22:36:15 +1300

    Add no mootools loader

---
+++ eslint-rules/no-mootools.js
@@ -0,0 +1,41 @@
+const disallowed = [{
+  node: 'Object',
+  // TODO: Check for ES version for Object.values()
+  methods: {
+    core: [
+      'each', 'merge', 'clone', 'append', 'subset', 'map', 'filter', 'every', 'some',
+      'getLength', 'contains', 'keyOf', 'toQueryString',
+      // TODO: Deprecated methods
+    ],
+    more: [
+      'getFromPath', 'cleanValues', 'erase', 'run',
+    ]
+  },
+}, {
+  node: 'Number',
+  methods: {
+    core: [
+      'convert', 'random', 'limit', 'round', 'times', 'toFloat', 'toInt',
+      // TODO: Disallow on number type too
+      'abs', 'acos', 'asin', 'atan2', 'ceil', 'cos', 'exp', 'floor', 'log', 'max', 'min',
+      'pow', 'sin', 'sqrt', 'tan'
+    ]
+  }
+}]
+
+module.exports = {
+    create: (context) => ({
+        MemberExpression: (node) => {
+            disallowed.forEach((d) => {
+              if (node.object.name === d.node) {
+                if (d.methods.core && d.methods.core.indexOf(node.property.name) > -1) {
+                  context.report(node.property, `Do not use MooTools Core ${d.node} method ${node.property.name}`);
+                }
+                if (d.methods.more && d.methods.more.indexOf(node.property.name) > -1) {
+                  context.report(node.property, `Do not use MooTools More ${d.node} method ${node.property.name}`);
+                }
+              }
+            });
+        }
+    })
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+const mootools = require('mootools');
+
+const obj = {a: 1, b: 2, c: 3};
+
+Object.toQueryString(obj);
+Object.contains(obj, 2);
+Object.keyOf(obj);
+
+console.log(Number.random(1, 10));
+
+// console.log((-10).abs());
