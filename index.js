eecd488c3ce12e968d4ce08f77006fed62746362
jrwebdev <web@jravenscroft.co.uk>
2017-01-12 23:26:44 +1300

    Move to plugin

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mootools = require('mootools');
 
 const obj = {a: 1, b: 2, c: 3};
 
+Object.prototype.abc = 123;
+
 Object.toQueryString(obj);
 Object.contains(obj, 2);
 Object.keyOf(obj);
@@ -9,8 +11,8 @@ Object.keyOf(obj);
 Number.random();
 Number.random();
 
-Number.limit(1, 20);
+console.log((-10).abs());
 
 console.log(Number.random(1, 10));
 
-// console.log((-10).abs());
+console.log((-10).abs());
